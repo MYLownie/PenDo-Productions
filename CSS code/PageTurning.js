a8d9390d4d4f97dc0423cdffffd5cd2eba784915
peterdo1216 <67163917+peterdo1216@users.noreply.github.com>
2020-08-06 13:38:35 -0400

    Added JS code for image loading

---
+++ CSS code/PageTurning.js
@@ -0,0 +1,7 @@
+var img = document.getElementById('id1');
+var newImg = new Image;
+newImg.onload = function(){
+    img.src= this.src;
+
+}
+newImg.src = "https://raw.githubusercontent.com/MYLownie/PenDo-Productions/master/images/Sample%20Page%20Crop.png"
