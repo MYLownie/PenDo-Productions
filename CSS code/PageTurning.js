d509b5d7a214e804e9934edd2f6723079ff63c38
peterdo1216 <67163917+peterdo1216@users.noreply.github.com>
2020-08-06 13:49:52 -0400

    Update PageTurning.js

--- CSS code/PageTurning.js
+++ CSS code/PageTurning.js
@@ -1,7 +1,3 @@
-var img = document.getElementById('id1');
-var newImg = new Image;
-newImg.onload = function(){
-    img.src= this.src;
-
-}
-newImg.src = "https://raw.githubusercontent.com/MYLownie/PenDo-Productions/master/images/Sample%20Page%20Crop.png" ;
+var img = new Image;
+img.addEventListener('load', function(){},false);
+img.src = "https://raw.githubusercontent.com/MYLownie/PenDo-Productions/master/images/Sample%20Page%20Crop.png" ;
