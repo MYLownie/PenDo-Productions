e238fa862fde79afe89bca7f84ed46144d2bb936
peterdo1216 <67163917+peterdo1216@users.noreply.github.com>
2020-08-06 13:59:06 -0400

    Trying to preload images

--- CSS code/PageTurning.js
+++
@@ -1,3 +0,0 @@
-var img = new Image;
-img.addEventListener('load', function(){},false);
-img.src = "https://raw.githubusercontent.com/MYLownie/PenDo-Productions/master/images/Sample%20Page%20Crop.png" ;
