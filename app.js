90a108fa6251c957da810bdbe1aee93eeb8e76b5
peterdo1216 <67163917+peterdo1216@users.noreply.github.com>
2020-08-13 14:07:56 -0400

    Uploading Image
    You can ignore the code

--- app.js
+++ app.js
@@ -95,30 +95,79 @@ function chapterChange(){
 }
 
 //be scroll mode when checked and click mode when unchecked
-const viewMode = document.getElementById('scrollvsclick');
-console.log(viewMode);
+const vm = document.getElementById('scrollvsclick');
+//console.log(viewMode);
 var scrollMode = document.getElementById('ib');
 var clickMode = document.getElementById('ic');
-viewMode.addEventListener('change', function(e) {
-  if(viewMode.checked){
+vm.addEventListener('change', function(e) {
+  if(vm.checked){
     //Click enabled
     scrollMode.style.display = "none";
     clickMode.style.display = "block";
     scroll(0,0);
     ct =true; // if Click is set, ct is true
-    console.log(ct);
   } else {
     //Scroll enabled
     scrollMode.style.display = "block";
     clickMode.style.display = "none";
     ct =false; // check if its on scroll to change the Page counter
-    console.log(ct);  
-    //Checks for things in the Viewport
   }
 });
+/*
+//Testing Viewport stuff
+const numSteps = 20.0;
 
+let boxElement;
+let prevRatio = 0.0;
+let increasingColor = "rgba(40, 40, 190, ratio)";
+let decreasingColor = "rgba(190, 40, 40, ratio)";
 
+// Set things up
+window.addEventListener("load", (event) => {
+  boxElement = document.querySelector("#box");
 
+  createObserver();
+}, false);
+
+function createObserver() {
+  let observer;
+
+  let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: buildThresholdList()
+  };
+
+  observer = new IntersectionObserver(handleIntersect, options);
+  observer.observe(boxElement);
+}
+
+function buildThresholdList() {
+  let thresholds = [];
+  let numSteps = 20;
+
+  for (let i=1.0; i<=numSteps; i++) {
+    let ratio = i/numSteps;
+    thresholds.push(ratio);
+  }
+
+  thresholds.push(0);
+  return thresholds;
+}
+
+function handleIntersect(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > prevRatio) {
+      entry.target.style.backgroundColor = increasingColor.replace("ratio", entry.intersectionRatio);
+    } else {
+      entry.target.style.backgroundColor = decreasingColor.replace("ratio", entry.intersectionRatio);
+    }
+
+    prevRatio = entry.intersectionRatio;
+  });
+}
+
+*/
 
 
 
