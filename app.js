fbfc039b35285292265aa688582c9ec439e6d44b
MYLownie <66714911+MYLownie@users.noreply.github.com>
2020-09-16 16:32:00 -0400

    Complete Reupload
    Moved all old files into a new folder called "Out of Date" so they weren't lost. Replaced with new index, light and dark theme css and new app.js file. Will most likely need to fiddle with a new imaged folder and commit again

--- app.js
+++ app.js
@@ -1,166 +1,44 @@
-src="https://code.jquery.com/jquery-2.2.3.min.js";
-//For Navigation Burger Drop Down
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
 
-    
-    burger.addEventListener('click',()=>{
-        //Toggle Nav
-        nav.classList.toggle('nav-active');
+//LIGHT MODE || DARK MODE
+//toggles between two diff stylesheets when sun svg is clicked
 
-        //Animate Links
-        navLinks.forEach((link, index)=>{
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`; 
-            }
-           
-        });
+//grabs the sun svg and the stylesheet link
+const sunBtn = document.querySelector("#sun");
 
-        //Burger Animation
-        burger.classList.toggle('toggle');
-    
-    });
-}
-navSlide();
+const theme = document.querySelector("#theme-link");
 
-//For Chapter Navigation Dropdown Menu
-//when user click on dropdown, toggle btween hiding and showing the dropdown content
-function chapDropdown() {
-    document.getElementById("chapterDrop").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
+
+//if the sun is clicked and the the stylesheet link's href is already light theme, set to dark, else set to light
+sunBtn.addEventListener("click", function() {
+    if (theme.getAttribute("href") == "light-theme.css") {
+        theme.href = "dark-theme.css";
+    } else {
+        theme.href = "light-theme.css";
     }
-  }
-
-
-  //SAME FUNCTION AS ^^ just using the Image button instead of the Chapter 1 button.
-function miniDropdown() {
-  var miniMenu = document.getElementById("menuDrop");
-  miniMenu.classList.toggle("appear")  
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dragonbtn')) {
-      var dropdowns = document.getElementsByClassName("miniDrop-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('appear')) {
-          openDropdown.classList.remove('appear');
-        }
-      }
-    }
-  }
-
-
-
-
-  //FOR STICK chapNAV
-  window.onscroll = function() {stickyNav()};
-
-  var navbar = document.getElementById("chapNav");
-  var sticky = navbar.offsetTop; //sets sticky to the distance of the chapNav from the top of it's parent node
-  var logo = document.getElementsByClassName("dragonbtn");
-  var contentbar = document.getElementById("menuDrop");
-  console.log(contentbar);
-
-  function stickyNav() {
-      if (window.pageYOffset >= sticky) {
-          navbar.classList.add("sticky");
-          logo[0].classList.add("active");
-      } else {
-          navbar.classList.remove("sticky");
-          logo[0].classList.remove("active");
-          contentbar.classList.remove("appear");
-      }
-  }
-  
-
-//My master piece of changing page and chapters
-  var pc = 1; //Counts the pages
-  var min = 6; //Amount of pages -1
-  var max = 7; //Amount of pages
-  var nc= max+1; //This countrols the chapter update at end of chapter
-  var maxC = 3; //Update with newest Chapter amount
-  var ct =true; //Checks if click to turn is on
-    function changeImage() {
-      // if(pc<=min){
-      //   pc++;
-      //   document.getElementById("ci").src = "Chapter"+cc+"/page"+pc+".jpg";
-      //   if(ct==true){//This updates the Page number
-      //     document.getElementById("pgn").textContent = pc;
-      //   }
-      //   //window.open("Chapter2.html","_self");
-      // }else if(pc>=max){
-      //   pc++;
-      //   cc++;
-      // }
-      // if(pc>=nc){
-      //     if(cc>=maxC){
-      //         cc=1;
-      //     }
-      //   document.getElementById("ci").onclick = chapterChange();
-      // }
-      if(pc<=min){
-        pc++;
-        document.getElementById("ci").src = "Test"+cc+"/Test"+pc+".png";
-        if(ct==true){//This updates the Page number
-          document.getElementById("pgn").textContent = pc;
-        }
-        //window.open("Chapter2.html","_self");
-      }else if(pc>=max){
-        pc++;
-      }
-      if(pc>=nc){
-          if(cc>=maxC){
-              cc=1;
-          }
-        document.getElementById("ci").onclick = chapterChange();
-      }
-}
-function chapterChange(){
-  //document.getElementById("ci").src = "Chapter1/EndOfChapter"+pc+".jpg";
-  window.open("chap"+ cc + ".html", "_self");
-}
-
-
-
-//be scroll mode when checked and click mode when unchecked
-const vm = document.getElementById('scrollvsclick');
-var scrollMode = document.getElementById('ib');
-console.log(scrollMode);
-var clickMode = document.getElementById('ic');
-vm.addEventListener('change', function(e) {
-  if(vm.checked){
-    //Click enabled
-    scrollMode.style.display = "none";
-    clickMode.style.display = "block";
-    scroll(0,0);
-    ct =true; // if Click is set, ct is true
-  } else {
-    //Scroll enabled
-    scrollMode.style.display = "block";
-    clickMode.style.display = "none";
-    ct =false; // check if its on scroll to change the Page counter
-  }
-  
 });
 
-//Storing Data in Local Storage
 
+//Hidden Burger Menu appear and disappear function
+const menuAppear = () => {
+
+    //selects the burger svg and the hidden menu element
+    const burger = document.getElementById('burger');
+    const hiddenMenu = document.getElementsByClassName('hidden-menu');
+
+    //uses a loop because the hidden menu is an array
+    //toggles a new "burger-active" class to the buttons when burger is clicked
+    burger.addEventListener('click',()=>{
+        for (a = 0; a < hiddenMenu.length; a++) {
+            hiddenMenu[a].classList.toggle('burger-active');
+        }
+    });
+}
+//function must be called
+menuAppear();
+
+
+//two different values, top for Safari, bottom for others (chrome etc.)
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
