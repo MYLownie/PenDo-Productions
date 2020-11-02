6aaba7571498c6a1039e76e942fb588a03f9b7f7
MYLownie <66714911+MYLownie@users.noreply.github.com>
2020-11-02 11:50:53 -0500

    ViewBox

--- myapp-2.0.js
+++ myapp-2.0.js
@@ -103,6 +103,7 @@ const goBack = () => {
 }
 goBack();
 
+
 const drawerShut = () => {
     backScrim[0].addEventListener('click', ()=>{
     for (a = 0; a <mobileHidMenu.length; a++) {
@@ -117,6 +118,7 @@ const drawerShut = () => {
 });
 }
 drawerShut();
+
 
 const coverOne = document.getElementById('comic-one');
 const popUp1 = document.getElementsByClassName('firstcomic-popup')
@@ -162,36 +164,3 @@ window.addEventListener("scroll", function() {
     }
     lastScrollTop = scrollTop;
 });
-
-//BUTTON RIPPLE EFFECT
-function createRipple(event) {
-    const button = event.currentTarget;
-  
-    const circle = document.createElement("span");
-    const diameter = Math.max(button.clientWidth, button.clientHeight);
-    const radius = diameter / 2;
-  
-    circle.style.width = circle.style.height = `${diameter}px`;
-    circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
-    circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
-    circle.classList.add("ripple");
-  
-    const ripple = button.getElementsByClassName("ripple")[0];
-  
-    if (ripple) {
-      ripple.remove();
-    }
-  
-    button.appendChild(circle);
-  }
-
-const navBtns = document.getElementsByClassName("pv-nav-btns");
-
-console.log(navBtns);
-
-navBtns.addEventListener('click', createRipple());
-  
-//   const buttons = document.getElementsByTagName("button");
-//   for (const button of buttons) {
-//     button.addEventListener("click", createRipple);
-//   }
