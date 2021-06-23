a664773eb3923b74ee530f56ecebc49cae112546
MYLownie <66714911+MYLownie@users.noreply.github.com>
2021-06-22 21:07:01 -0400

    Adding Mobile for Infopage

--- ap_app.js
+++ ap_app.js
@@ -166,3 +166,77 @@ const cards = document.querySelectorAll('.ap_activeChara');
 		};
 
 		cards.forEach(card => card.addEventListener('click', onCardClick));
+
+// MOBILE ONLY FUNCTIONS
+
+//Mobile Burger menu appear function
+const mobileBurg = document.getElementById('burger');
+const mobileHidMenu = document.getElementsByClassName('hidden-menu-mobile');
+const backScrim = document.getElementsByClassName('scrim');
+const body = document.getElementsByTagName("BODY");
+
+
+//open the side drawer
+const mobileDrawer = () => {
+burger.addEventListener('click', ()=>{
+    for (a = 0; a <mobileHidMenu.length; a++) {
+        mobileHidMenu[a].classList.toggle('is-open');
+        backScrim[a].classList.toggle('closed-scrim');
+        body[0].classList.toggle('locked-scroll');
+    }
+});
+}
+mobileDrawer();
+
+
+const storyOp1 = document.getElementsByClassName('story1');
+const storyOp2 = document.getElementsByClassName('story2');
+const story1Menu = document.getElementsByClassName ('story1-menu-mobile');
+const story2Menu = document.getElementsByClassName ('story2-menu-mobile');
+
+
+const story1Drawer = () => {
+    storyOp1[0].addEventListener('click', ()=>{
+        for(a = 0; a < story1Menu.length; a++)  {
+            story1Menu[a].classList.add('open-drawer');
+        }
+    });
+}
+story1Drawer();
+
+const story2Drawer = () => {
+    storyOp2[0].addEventListener('click', ()=> {
+        for(i = 0; i < story2Menu.length; i++)  {
+            story2Menu[i].classList.add("open-drawer");
+        }
+    });
+}
+story2Drawer();
+
+var backBtn = document.getElementsByClassName('drawer-back');
+
+const goBack = () => {
+    backBtn[0].addEventListener('click', () => {
+        story1Menu[0].classList.remove('open-drawer');
+    });
+    backBtn[1].addEventListener('click', () => {
+        story2Menu[0].classList.remove('open-drawer');
+    });
+}
+goBack();
+
+
+const drawerShut = () => {
+    backScrim[0].addEventListener('click', ()=>{
+    for (a = 0; a <mobileHidMenu.length; a++) {
+        if (story1Menu[0].classList.contains('open-drawer') || story2Menu[0].classList.contains('open-drawer')) {
+            pass
+        } else {
+            mobileHidMenu[a].classList.toggle('is-open');
+            backScrim[a].classList.toggle('closed-scrim');
+            body[0].classList.toggle('locked-scroll');
+        }   
+    }
+});
+}
+drawerShut();
